aad1871623279b5be352a1325cad89342475da80
Przemek <przemek.grzywa@gmail.com>
2025-09-19 13:30:23 +0200

    Updated definitions. Add gates support.

--- compit_inext_api/connector.py
+++ compit_inext_api/connector.py
@@ -4,7 +4,7 @@ import logging
 from compit_inext_api.api import CompitAPI
 from compit_inext_api.consts import CompitParameter
 from compit_inext_api.device_definitions import DeviceDefinitionsLoader
-from compit_inext_api.types.DeviceState import DeviceInstance, DeviceState, Param
+from compit_inext_api.types.DeviceState import DeviceInstance, DeviceState, GateInstance, Param
 
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
@@ -13,10 +13,23 @@ _LOGGER: logging.Logger = logging.getLogger(__package__)
 class CompitApiConnector:
     """Connector class for Compit API."""
 
-    devices: dict[int, DeviceInstance] = {}
+    gates: dict[int, GateInstance] = {}
+
+    @property
+    def all_devices(self) -> dict[int, DeviceInstance]:
+        devices = {}
+        for gate in self.gates.values():
+            devices.update(gate.devices)
+        return devices
 
     def __init__(self, session: aiohttp.ClientSession) -> None:
         self.session = session
+
+    def get_device(self, device_id: int) -> DeviceInstance | None:
+        for gate in self.gates.values():
+            if device_id in gate.devices:
+                return gate.devices[device_id]
+        return None
 
     async def init(self, email: str, password: str, lang: str = "en") -> bool:
         self.api = CompitAPI(email, password, self.session)
@@ -26,12 +39,13 @@ class CompitApiConnector:
             return False
         
         for gates in self.systemInfo.gates:
+            self.gates[gates.id] = GateInstance(gates.id, gates.label)
             for device in gates.devices:
                 try:
-                    self.devices[device.id] = DeviceInstance(await DeviceDefinitionsLoader.get_device_definition(device.type, lang)) 
+                    self.gates[gates.id].devices[device.id] = DeviceInstance(device.label, await DeviceDefinitionsLoader.get_device_definition(device.type, lang))
                     state = await self.api.get_state(device.id)
                     if state and isinstance(state, DeviceState):
-                        self.devices[device.id].state = state
+                        self.gates[gates.id].devices[device.id].state = state
                     else:
                         _LOGGER.error("Failed to get state for device %s", device.id)
                 except ValueError:
@@ -40,11 +54,12 @@ class CompitApiConnector:
 
     async def update_state(self, device_id: int | None) -> None:
         if device_id is None:
-            for device in self.devices.keys():
-                await self.update_state(device)
+            for gate in self.gates.values():
+                for device in gate.devices.keys():
+                    await self.update_state(device)
             return
 
-        device = self.devices.get(device_id)
+        device = self.get_device(device_id)
         if device is None:
             _LOGGER.warning("No device found with ID %d", device_id)
             return
@@ -56,11 +71,17 @@ class CompitApiConnector:
             _LOGGER.error("Failed to get state for device %s", device_id)
 
     def get_device_parameter(self, device_id: int, parameter: CompitParameter) -> Param | None:
-         return self.devices[device_id].state.get_parameter_value(parameter.value)
-    
+        device = self.get_device(device_id)
+        if device:
+            return device.state.get_parameter_value(parameter.value)
+        return None
+
     async def set_device_parameter(self, device_id: int, parameter: CompitParameter, value: str | float) -> bool:
         result = await self.api.update_device_parameter(device_id, parameter, value)
         if result:
-            self.devices[device_id].state.set_parameter_value(parameter.value, value)
-            return True
-        return False
+            device = self.get_device(device_id)
+            if device:
+                device.state.set_parameter_value(parameter.value, value)
+        return result
+
+        
